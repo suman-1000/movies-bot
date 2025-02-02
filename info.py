7494e0895320e821dc3b28811b7d98097a2b9a77
suman-1000 <sumangayen1000@gmail.com>
2025-02-02 15:56:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -59,9 +59,9 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sumangayen1000:y4WipxQtAnTW3eQf@cluster0.7omv1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://movie1:movie1@movie1.ifhuc.mongodb.net/?retryWrites=true&w=majority&appName=movie1")   # IF Multiple Database Is False Then Fill Only This Database Url.
+DATABASE_NAME = environ.get('DATABASE_NAME', "movie1")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'movie1')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
