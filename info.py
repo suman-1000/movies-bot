fc1025049375edeb57380b9720472f3b258619cb
suman-1000 <sumangayen1000@gmail.com>
2025-02-07 01:38:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -88,9 +88,9 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 
 # Links
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movies_request1234')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/movies_request1234')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/movies_request1234') # Support Chat Link Without https:// or @
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+MnYsWkmvtfNhMjc1')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+MnYsWkmvtfNhMjc1')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+MnYsWkmvtfNhMjc1') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', '@suman_only')
 
 # True Or False
